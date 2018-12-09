3e11d157c33490833f02ebdd466dfe18c1636771
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-12-09 23:21:06 +0530

    Adding http get

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,36 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(rspns => {
+                const postswithauthor = rspns.data.map(post => {
+                    return {
+                        ...post,
+                        author: 'Arnab'
+                    }
+                })
+                this.setState({
+                    posts: [...this.state.posts],
+                    posts: postswithauthor
+                });
+            })
+            .catch(err => console.log(err));
+    }
+
+    render() {
         return (
             <div>
                 <section className="Posts">
+                    {this.state.posts.map(post => <Post key={post.id} title={post.title} author={post.author}></Post>)}
+                    {/* <Post />
                     <Post />
-                    <Post />
-                    <Post />
+                    <Post /> */}
                 </section>
                 <section>
                     <FullPost />
