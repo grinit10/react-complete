66a537b7eda271422bb307eac9468b43d8b0e1af
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-12-14 19:27:15 +0530

    Adding basic redux counter

---
+++ src/store/reducer.js
@@ -0,0 +1,39 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+
+    switch(action.type) {
+        case 'INCREMENT':
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+        case 'DECREMENT':
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+        case 'ADD':
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+        case 'SUB':
+        return {
+            ...state,
+            counter: state.counter - action.value
+        }
+    }
+
+    if (action.type === 'INCREMENT') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    return state;
+};
+
+export default reducer;
