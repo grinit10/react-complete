4242beb1925f0952074422dede337a07ff1a166a
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2019-02-27 09:39:58 +0530

    Adding initial version

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+
+import Header from './components/Header';
+import ImageGrid from './components/ImageGrid';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi, I'm a React App</h1>
-       <p>This is really working!</p>
-       <Person name="Max" age="28" />
-       <Person name="Manu" age="29" >My Hobbies: Racing</Person>
-       <Person name="Stephanie" age="26" />
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <ImageGrid />
+            </div>
+        );
+    }
 }
 
 export default App;
