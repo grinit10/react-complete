757cb709d1e5f38fd214f7ed345afc45b51ac65e
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2019-02-28 15:50:04 +0530

    Adding redux saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 import rootReducer from './reducers/index.reducer';
+import helloSaga from './sagas/index';
+
+const sagaMiddleware = createSagaMiddleware()
 
 const configureStore = () => {
-    return createStore(rootReducer);
+    const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+    store.dispatch({type:'Hello'})
+    sagaMiddleware.run(helloSaga)
+    return store;
 }
 export default configureStore;
